c38e6b6a9a64b16cd3710c7efa4ae9a3150868b5
Brandon Boey <brandonjboey@gmail.com>
2020-01-13 17:41:48 -0500

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Brandon Boey on 1/13/20.
+//  Copyright © 2020 Brandon Boey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
