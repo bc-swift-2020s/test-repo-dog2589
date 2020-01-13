4cbc2a0f51e9c5add966c07c4f8c3d8197a34b9c
Brandon Boey <brandonjboey@gmail.com>
2020-01-13 18:37:00 -0500

    Weekone

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sayitPressed(_ sender: UIButton) {
+        messageLabel.text = "Swiftacular"
+    }
+    
 }
 
